15f68d9237d1abc85c304acc9653720d35faf01d
peter03x <103523966@student.swin.edu.au>
2023-03-14 02:08:42 +1100

    stll week 2

---
+++ Implementation Project/Swin-Adventure/Swin-Adventure/IdentifiableObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swin_Adventure
+{
+    internal class IdentifiableObject
+    {
+        private List<string> _identifiers;
+        public  IdentifiableObject(string[] idents)
+        {
+            _identifiers.AddRange(idents);
+        }
+        public bool AreYou(string id)
+        {
+            foreach(string c in _identifiers) 
+            { 
+                if (c == id) return true;
+            }
+            return false;
+        }
+        public string FirstId
+        { 
+            get
+            {
+                if (_identifiers == null) return ""; 
+                else return _identifiers.First();
+            }
+        }
+        public void AddIdentifier(string id)
+        {
+            int i = 0;
+            foreach (string c in _identifiers)
+            {
+                ++i;
+                if (c == id)
+                {
+                    _identifiers.Remove(c);
+                    _identifiers.Insert(i, id.ToLower());
+                    break;
+                }
+            }
+            i = 0;
+        }
+    }
+}
---
+++ Implementation Project/Swin-Adventure/Swin-Adventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Swin_Adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
